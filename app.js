0791469f066705769df43925c716f72a97e37042
Sean Park <chanseanpark@gmail.com>
2020-09-13 23:06:21 -0700

    updated URIs and callback urls for authetication.

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://secretsdemo.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -90,7 +90,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets",
+    callbackURL: "https://secretsdemo.herokuapp.com/auth/facebook/secrets",
     enableProof: true
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -207,12 +207,6 @@ app.post('/submit', (req, res) => {
     }
   });
 });
-
-
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
